4d7e25ff2e16f2120c3790b8b8ea9281f75bd7a1
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-15 19:32:21 +0200

    ex1

--- main.py
+++ main.py
@@ -19,3 +19,29 @@ obj = D()
 obj.say_hello()
 print(D.mro())
 print(D.__mro__)
+
+#ex1
+class C:
+    def __init__(self):
+        print('c')
+
+
+class A:
+    def __init__(self):
+        print('a')
+
+
+class B(A):
+    def __init__(self):
+        super().__init__()
+        print('b')
+
+
+class D(B, A):
+    def __init__(self):
+        super().__init__()
+        # A.__init__(self)
+        print('d')
+
+
+obj = D()
